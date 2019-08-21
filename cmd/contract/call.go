d24c75e764984c591f3a7c0dacb45e3f2b6fcf90
xiaohuo <xiaohuo200@gmail.com>
2019-08-21 18:30:35 +0800

    chore: fall back contract paramater

--- cmd/contract/call.go
+++ cmd/contract/call.go
@@ -25,8 +25,8 @@ func init() {
 	callCmd.Flags().Int64VarP(&invokePrice, "price", "p", 10000000000, "set gas price")
 	callCmd.Flags().Int32VarP(&invokeLimit, "limit", "l", 10000, "set gas limit")
 	callCmd.Flags().StringVarP(&invokeAddress, "address", "a", "", "smart contract address")
-	callCmd.Flags().IntVarP(&chainId, "chainId", "d", 333, "chain id")
-	callCmd.Flags().StringVarP(&api, "api", "u", "https://dev-api.zilliqa.com/", "api url")
+	callCmd.Flags().IntVarP(&chainId, "chainId", "d", 0, "chain id")
+	callCmd.Flags().StringVarP(&api, "api", "u", "", "api url")
 	ContractCmd.AddCommand(callCmd)
 }
 
