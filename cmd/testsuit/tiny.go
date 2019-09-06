87eeb4c78f7b52d9b7be10db9d44bfd06796185d
xiaohuo <xiaohuo200@gmail.com>
2019-09-06 18:18:24 +0800

    chore(testsuit): add two more tests for tiny contract

--- cmd/testsuit/tiny.go
+++ cmd/testsuit/tiny.go
@@ -17,7 +17,7 @@ import (
 
 var address string
 var wallet *core.Wallet
-var transitions = []string{"t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "t10", "t11", "t12", "t13", "t14", "t15", "t16"}
+var transitions = []string{"t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9", "t10", "t11", "t12", "t13", "t14", "t15", "t16", "t17", "t18"}
 
 func init() {
 	tinyCmd.Flags().StringVarP(&address, "address", "a", "", "the address of tiny contract")
