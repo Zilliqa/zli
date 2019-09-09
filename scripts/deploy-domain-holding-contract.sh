10631480a15aa601d433041568002c4aee8dc002
xiaohuo <xiaohuo200@gmail.com>
2019-09-09 19:12:56 +0800

    chore: update tests

--- scripts/deploy-domain-holding-contract.sh
+++ scripts/deploy-domain-holding-contract.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
- go-zli contract deploy -c ../testsuit/contracts/UnstoppableDomains/Holding.scilla -i ../testsuit/contracts/UnstoppableDomains/holdinginit.json -l 1000000 -p 1000000000000
+ go-zli contract deploy -c ../testsuit/contracts/UnstoppableDomains/Holding.scilla -i ../testsuit/contracts/UnstoppableDomains/holdinginit.json -l 10000
