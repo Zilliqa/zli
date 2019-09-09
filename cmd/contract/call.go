7b6ec9efae6d238d5f48f6878a6d617ccc0b29d6
xiaohuo <xiaohuo200@gmail.com>
2019-09-09 18:49:35 +0800

    chore: set default invoke limit

--- cmd/contract/call.go
+++ cmd/contract/call.go
@@ -27,7 +27,7 @@ func init() {
 	callCmd.Flags().StringVarP(&invokeTransition, "transition", "t", "", "transition will be called")
 	callCmd.Flags().StringVarP(&invokeArgs, "args", "r", "", "args will be passed to transition")
 	callCmd.Flags().Int64VarP(&invokePrice, "price", "p", 1000000000, "set gas price")
-	callCmd.Flags().Int32VarP(&invokeLimit, "limit", "l", 10, "set gas limit")
+	callCmd.Flags().Int32VarP(&invokeLimit, "limit", "l", 10000, "set gas limit")
 	callCmd.Flags().StringVarP(&invokeAddress, "address", "a", "", "smart contract address")
 	callCmd.Flags().IntVarP(&chainId, "chainId", "d", 0, "chain id")
 	callCmd.Flags().StringVarP(&api, "api", "u", "", "api url")
@@ -99,7 +99,7 @@ var callCmd = &cobra.Command{
 			Version:      strconv.FormatInt(int64(LaksaGo.Pack(wallet.ChainID, 1)), 10),
 			Nonce:        strconv.FormatInt(nonce+1, 10),
 			GasPrice:     strconv.FormatInt(price, 10),
-			GasLimit:     strconv.FormatInt(int64(limit), 10),
+			GasLimit:     strconv.FormatInt(int64(invokeLimit), 10),
 			SenderPubKey: strings.ToUpper(wallet.DefaultAccount.PublicKey),
 			Amount:       invokeAmount,
 		}
