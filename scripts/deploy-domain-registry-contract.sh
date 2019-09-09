93e8383bb8d4b086ea32a370ff02dc05d7cd1c6c
xiaohuo <xiaohuo200@gmail.com>
2019-09-09 19:12:56 +0800

    chore: update tests

--- scripts/deploy-domain-registry-contract.sh
+++ scripts/deploy-domain-registry-contract.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
- go-zli contract deploy -c ../testsuit/contracts/UnstoppableDomains/Registry.scilla -i ../testsuit/contracts/UnstoppableDomains/registryinit.json -l 1000000 -p 1000000000000
+ go-zli contract deploy -c ../testsuit/contracts/UnstoppableDomains/Registry.scilla -i ../testsuit/contracts/UnstoppableDomains/registryinit.json -l 100000
