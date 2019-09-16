d6653f168fe00d0f5ffc33b9925391e836eb1ec8
xiaohuo <xiaohuo200@gmail.com>
2019-09-16 14:38:40 +0800

    chore: set tiny test's gas price to default

--- scripts/deploy-tiny-contract.sh
+++ scripts/deploy-tiny-contract.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
- go-zli contract deploy -c ../testsuit/contracts/TinyContract/tiny.scilla -i ../testsuit/contracts/TinyContract/init.json -l 100000
+ go-zli contract deploy -c ../testsuit/contracts/TinyContract/tiny.scilla -i ../testsuit/contracts/TinyContract/init.json -l 120000
+
