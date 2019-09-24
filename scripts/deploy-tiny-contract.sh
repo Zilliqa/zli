2e86798a0aef16bb8e74f637fc8cbef74e8066f2
xiaohuo <xiaohuo200@gmail.com>
2019-09-24 13:31:32 +0800

    feat(transfer): support specify a private key for the transfer

--- scripts/deploy-tiny-contract.sh
+++ scripts/deploy-tiny-contract.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
- go-zli contract deploy -c ../testsuit/contracts/TinyContract/tiny.scilla -i ../testsuit/contracts/TinyContract/init.json -l 120000
+go-zli contract deploy -c ../testsuit/contracts/TinyContract/tiny.scilla -i ../testsuit/contracts/TinyContract/init.json -l 120000
 
