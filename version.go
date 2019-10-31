8c14a10fb4a2fbda7d939f6e816cd82530352518
xiaohuo <xiaohuo200@gmail.com>
2019-10-31 18:18:25 +0800

    chore: upgrade version

--- version.go
+++ version.go
@@ -1,6 +1,6 @@
 package zli
 
 // CurrentVersionNumber is the current application's version literal
-const CurrentVersionNumber = "0.0.5"
+const CurrentVersionNumber = "0.0.6"
 
 const ApiVersion = "/zli/" + CurrentVersionNumber + "/"
