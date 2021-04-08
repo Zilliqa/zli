2283daba5e4823dcc049afc897573a08bb729a30
xiaohuo <xiaohuo200@gmail.com>
2021-04-08 18:30:53 +0800

    feat: add proof support

---
+++ cmd/proof/proof.go
@@ -0,0 +1,12 @@
+package proof
+
+import "github.com/spf13/cobra"
+
+var ProofCmd = &cobra.Command{
+	Use:   "proof",
+	Short: "proof tool",
+	Long:  "proof tool",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
